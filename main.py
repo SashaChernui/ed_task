cb710f2f93673397193541fbc277d916b253c54b
SashaChernui <143911169+SashaChernui@users.noreply.github.com>
2023-12-07 01:49:58 +0300

    Add main

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
+numbers = []
+
+number = int(input('Введите число:'))
+num = 1
+
+if number == 1:
+   num += 1
+else:
+   num = number
+
+for i in range (1,num,2):
+    numbers.append(i)
+
+print('Список из нечётных чисел от 1 до',number,':',numbers)
 
 
